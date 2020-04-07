ed550bd47fa1549145d5e0890acc07c5a1ebb93d
Leana N <leananeparidze@gmail.com>
2020-04-07 16:17:11 -0700

    try cors filter

--- src/main/java/com/project/todo/config/SecurityConfig.java
+++ src/main/java/com/project/todo/config/SecurityConfig.java
@@ -30,31 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     @Resource(name = "securityUserService")
     private UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().
-                authorizeRequests()
-                .requestMatchers(CorsUtils:: isPreFlightRequest).permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .realmName("Biometrix");
-
-        http.csrf().disable();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
     @Override
     @Bean
